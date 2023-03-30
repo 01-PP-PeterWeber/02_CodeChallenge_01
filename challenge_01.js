9e91f2981ec5105c00daa5127f01ff1edfb8591d
Peter Weber <peter-aalen@gmx.net>
2023-03-30 14:57:14 +0200

    ZeilenInfo ++

--- challenge_01.js
+++ challenge_01.js
@@ -9,12 +9,16 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+/** fordert den Benutzer auf, eine Zahl einzugeben und speichert sie in einer Variablen **/
 let zahl1 = prompt("Geben Sie die erste Zahl ein:");
 
+/** fordert den Benutzer auf, eine zweite Zahl einzugeben und speichert sie in einer Variablen **/
 let zahl2 = prompt("Geben Sie die zweite Zahl ein:");
 
+/** konvertiert die Eingabezeichenfolgen in Zahlen und berechnet die Summe **/
 let summe = parseFloat(zahl1) + parseFloat(zahl2);
 
+/** gibt die Summe der beiden Zahlen in die Konsole aus **/
 console.log("Die Summe der Zahlen ist: " + summe);
 
 
