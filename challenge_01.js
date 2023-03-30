4fbe7d59f15bff33e3f9eabcc70896aa05e7658b
Peter Weber <peter-aalen@gmx.net>
2023-03-30 14:33:00 +0200

    Challegen Win

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,13 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let zahl1 = prompt("Geben Sie die erste Zahl ein:");
+
+let zahl2 = prompt("Geben Sie die zweite Zahl ein:");
+
+let summe = parseFloat(zahl1) + parseFloat(zahl2);
+
+console.log("Die Summe der Zahlen ist: " + summe);
+
+
+
