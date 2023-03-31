cb1d424501a36c5ff45b8bea308a7ace4652b27f
Peter Weber <peter-aalen@gmx.net>
2023-03-31 09:39:04 +0200

    draw.io ++

--- challenge_01.js
+++ challenge_01.js
@@ -10,16 +10,16 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 /** fordert den Benutzer auf, eine Zahl einzugeben und speichert sie in einer Variablen **/
-let zahl1 = prompt("Geben Sie die erste Zahl ein:");
+// let zahl1 = prompt("Geben Sie die erste Zahl ein:");
 
 /** fordert den Benutzer auf, eine zweite Zahl einzugeben und speichert sie in einer Variablen **/
-let zahl2 = prompt("Geben Sie die zweite Zahl ein:");
+// let zahl2 = prompt("Geben Sie die zweite Zahl ein:");
 
 /** konvertiert die Eingabezeichenfolgen in Zahlen und berechnet die Summe **/
-let summe = parseFloat(zahl1) + parseFloat(zahl2);
+// let summe = parseFloat(zahl1) + parseFloat(zahl2);
 
 /** gibt die Summe der beiden Zahlen in die Konsole aus **/
-console.log("Die Summe der Zahlen ist: " + summe);
+// console.log("Die Summe der Zahlen ist: " + summe);
 
 
 
